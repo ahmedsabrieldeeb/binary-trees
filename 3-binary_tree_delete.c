62549669a36e441d5d70163f23c6c9491f7bfc54
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-11-27 18:21:37 +0200

    Delete Task: removed redundant if statements

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -14,16 +14,7 @@ void binary_tree_delete(binary_tree_t *tree)
 	if (tree == NULL)
 		return;
 
-	/* stick to the left most leaf until finding NULL */
-	if (tree->left != NULL)
-	{
-		binary_tree_delete(tree->left);
-	}
-
-	/* stick to the right most leaf until finding NULL */
-	if (tree->right != NULL)
-		binary_tree_delete(tree->right);
-
-	/* delete the node has no right nor left child */
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
 	free(tree);
 }
