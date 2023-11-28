de870f334d64ded99b0495202afe3cdff34929df
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-11-28 23:54:02 +0200

    Insert left/right Tasks: free the memory

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -16,9 +16,14 @@
 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node = NULL;
 
-	if (parent == NULL || new_node == NULL)
+	if (parent == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(binary_tree_t));
+
+	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = value;
