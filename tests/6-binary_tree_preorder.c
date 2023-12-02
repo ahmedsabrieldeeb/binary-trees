b1ad64a7f1c0ed0401a627c1c8ecef6ff71dbb9a
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-12-02 20:43:28 +0200

    6-binary_tree_preorder.c: betty issues

--- tests/6-binary_tree_preorder.c
+++ tests/6-binary_tree_preorder.c
@@ -4,13 +4,13 @@
 
 /**
  * binary_tree_preorder - goes through a binary tree using pre-order traversal
- * 
+ *
  * Description: If tree or func is NULL, do nothing
  *
  * @tree: pointer to the root node of the tree to delete
  * @func: pointer to a function to call for each node.
  * The value in the node must be passed as a parameter to this function.
- * 
+ *
 */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
@@ -19,7 +19,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 
 	if (tree != NULL)
 		func(tree->n);
-	
+
 	binary_tree_preorder(tree->left, func);
 	binary_tree_preorder(tree->right, func);
 }
